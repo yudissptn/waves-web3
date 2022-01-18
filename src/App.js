e2b29e43c77c4a92f6b99a41a76b2fc8c075bef7
Yudhistira Septian Sampoerno <yudissptn@gmail.com>
2022-01-18 21:07:18 +0700

    auth waves keeper

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const getPublicState = async () => {
+  try {
+    const state = await window.WavesKeeper.publicState();
+    console.log(state); // displaying the result in the console
+    /*... processing data*/
+  } catch (error) {
+    console.error(error); // displaying the result in the console
+    /*... processing errors */
+  }
+};
 
 function App() {
+  const authWavesKeeper = async () => {
+    if (window.WavesKeeper) {
+      await getPublicState();
+    } else {
+      alert("Install waves keeper extensions");
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Learn Web 3 on Waves</h1>
       </header>
+      <div>
+        <p>Press button to connect to waves wallet</p>
+        <button onClick={authWavesKeeper}>Auth</button>
+      </div>
     </div>
   );
 }
