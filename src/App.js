a5b5316911c1f0ed6d0e0113dfce0d62c982a7ee
Yudhistira Septian Sampoerno <yudissptn@gmail.com>
2022-01-18 21:32:04 +0700

    add connect state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
-const getPublicState = async () => {
-  try {
-    const state = await window.WavesKeeper.publicState();
-    console.log(state); // displaying the result in the console
-    /*... processing data*/
-  } catch (error) {
-    console.error(error); // displaying the result in the console
-    /*... processing errors */
-  }
-};
-
 function App() {
+  const [isConnected, setIsConnected] = useState({ state: false, address: "" });
+
+  const getPublicState = async () => {
+    try {
+      const state = await window.WavesKeeper.publicState();
+      console.log(state); // displaying the result in the console
+      /*... processing data*/
+      setIsConnected({ state: true, address: state.account.address });
+    } catch (error) {
+      console.error(error); // displaying the result in the console
+      /*... processing errors */
+    }
+  };
+
   const authWavesKeeper = async () => {
     if (window.WavesKeeper) {
       await getPublicState();
@@ -25,10 +29,14 @@ function App() {
       <header className="App-header">
         <h1>Learn Web 3 on Waves</h1>
       </header>
-      <div>
-        <p>Press button to connect to waves wallet</p>
-        <button onClick={authWavesKeeper}>Auth</button>
-      </div>
+      {!isConnected.state ? (
+        <div>
+          <p>Press button to connect to waves wallet</p>
+          <button onClick={authWavesKeeper}>Auth</button>
+        </div>
+      ) : (
+        <h3>Successfully connected to wallet: {isConnected.address}</h3>
+      )}
     </div>
   );
 }
