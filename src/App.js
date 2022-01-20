dd203c20d74eab8e91f24323f5824b405689156b
Yudhistira Septian Sampoerno <yudissptn@gmail.com>
2022-01-20 21:46:41 +0700

    add waves signer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import { useState } from "react";
 import "./App.css";
+import { Signer } from "@waves/signer";
+import { ProviderSeed } from "@waves/provider-seed";
+import { libs } from "@waves/waves-transactions";
+
+const seed = libs.crypto.randomSeed(15);
+const signer = new Signer({
+  // Specify URL of the node on Testnet
+  NODE_URL: "https://nodes-testnet.wavesnodes.com",
+});
+signer.setProvider(new ProviderSeed(seed));
 
 function App() {
   const [isConnected, setIsConnected] = useState({ state: false, address: "" });
+  const [isAuthSigner, setIsAuthSigner] = useState({
+    state: false,
+    address: "",
+  });
 
   const getPublicState = async () => {
     try {
@@ -24,19 +38,85 @@ function App() {
     }
   };
 
+  const authWavesSigner = async () => {
+    try {
+      const userData = await signer.login(); // calling Waves Signer
+      console.log(userData);
+      setIsAuthSigner({ state: true, address: userData.address });
+    } catch (e) {
+      console.error("login rejected"); // handling user auth reject
+    }
+  };
+
+  const invokeSigner = () => {
+    signer
+      .invoke({
+        dApp: "3MuN7D8r19zdvSpAd1L91Gs88bcgwUFy2mn",
+        fee: 0.004,
+        payment: [
+          {
+            amount: 677728840,
+            assetId: "WAVES",
+          },
+        ],
+        call: {
+          function: "faucet",
+          args: [
+            { type: "integer", value: 1 },
+            { type: "binary", value: "base64:AAA=" },
+            { type: "string", value: "foo" },
+          ],
+        },
+      })
+      .broadcast()
+      .then(console.log);
+  };
+
+  const sendDataSigner = () => {
+    signer
+      .data({
+        data: [{ key: "lastCall", value: String(new Date()), type: "string" }],
+      })
+      .broadcast()
+      .then(console.log);
+  };
+
+  const transferSigner = () => {
+    signer
+      .transfer({
+        recipient: "3MuN7D8r19zdvSpAd1L91Gs88bcgwUFy2mn",
+        amount: 1,
+      })
+      .broadcast()
+      .then(console.log);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Learn Web 3 on Waves</h1>
       </header>
-      {!isConnected.state ? (
+      <main>
+        {!isConnected.state ? (
+          <div>
+            <p>Press button to connect to waves keeper</p>
+            <button onClick={authWavesKeeper}>Auth</button>
+          </div>
+        ) : (
+          <h3>Successfully connected to wallet: {isConnected.address}</h3>
+        )}
         <div>
-          <p>Press button to connect to waves wallet</p>
-          <button onClick={authWavesKeeper}>Auth</button>
+          <h3>Waves Signer</h3>
+          {!isAuthSigner.state ? (
+            <button onClick={authWavesSigner}>Authorization</button>
+          ) : (
+            <p>{`Authorized as ${isAuthSigner.address}`}</p>
+          )}
+          <button onClick={invokeSigner}>Invoke Script</button>
+          <button onClick={sendDataSigner}>Send Data</button>
+          <button onClick={transferSigner}>Send Transfer</button>
         </div>
-      ) : (
-        <h3>Successfully connected to wallet: {isConnected.address}</h3>
-      )}
+      </main>
     </div>
   );
 }
